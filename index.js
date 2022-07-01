2489f15c44a8e90a995989d5b53164b7aac5458a
arnobtarafder <rmtarnob@gmail.com>
2022-07-01 17:59:45 +0600

    add: verification of jsonwebtoken data

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 
 app.use(cors());
@@ -12,7 +12,22 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hswxz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -211,7 +226,6 @@ async function run() {
         );
         res.send(taskUpdating);
       });
-
 
   } 
 
